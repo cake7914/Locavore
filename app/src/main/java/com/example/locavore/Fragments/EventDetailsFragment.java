fc534860ad3363f4dd61de7f71d5879f9c4fc048
Cierra Ouellette <cierra3202@gmail.com>
2022-07-18 11:26:11 -0700

    Users can view days/times for events in detailed view

--- app/src/main/java/com/example/locavore/Fragments/EventDetailsFragment.java
+++ app/src/main/java/com/example/locavore/Fragments/EventDetailsFragment.java
@@ -21,9 +21,12 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.parceler.Parcels;
 import org.w3c.dom.Text;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -32,8 +35,12 @@ public class EventDetailsFragment extends Fragment {
     TextView tvEventDescription;
     TextView tvLikeCount;
     TextView tvEventLocation;
+    TextView tvDaysOfWeek;
+    TextView tvDate;
+    TextView tvTime;
     ImageButton btnLike;
     Integer likeCount;
+
 
     public EventDetailsFragment() {
         // Required empty public constructor
@@ -58,10 +65,29 @@ public class EventDetailsFragment extends Fragment {
         tvEventLocation = view.findViewById(R.id.tvLocation);
         tvLikeCount = view.findViewById(R.id.tvLikeCount);
         btnLike = view.findViewById(R.id.btnLikeEvent2);
+        tvDate = view.findViewById(R.id.tvDates);
+        tvDaysOfWeek = view.findViewById(R.id.tvDaysOfWeek);
+        tvTime = view.findViewById(R.id.tvTime);
 
         tvEventName.setText(event.getName());
         tvEventDescription.setText(event.getDescription());
         tvEventLocation.setText(event.getLocationString());
+        Date startDate = event.getDate(Event.KEY_START_DATE);
+        Date endDate = event.getDate(Event.KEY_END_DATE);
+        tvDate.setText(startDate.getMonth() + "/" + startDate.getDay() + " - " + endDate.getMonth() + "/" + endDate.getDay());
+        tvTime.setText(startDate.getHours() + ":" + startDate.getMinutes() + " - " + endDate.getHours() + ":" + endDate.getMinutes());
+        JSONArray daysOfWeekJSON = event.getJSONArray(Event.KEY_DAYS_OF_WEEK);
+        String daysOfWeek = "";
+        for(int i = 0; i < 7; i++) {
+            try {
+                if(daysOfWeekJSON.getInt(i) == 1) {
+                    daysOfWeek += getDay(i);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        tvDaysOfWeek.setText(daysOfWeek);
 
         ParseQuery<UserEvent> userEventQuery = ParseQuery.getQuery("UserEvent");
         userEventQuery.whereEqualTo(UserEvent.KEY_EVENT_ID, event.getObjectId());
@@ -82,4 +108,23 @@ public class EventDetailsFragment extends Fragment {
 
     }
 
+    public String getDay(int pos) {
+        switch(pos) {
+            case 0:
+                return "M ";
+            case 1:
+                return "T ";
+            case 2:
+                return "W ";
+            case 3:
+                return "Th ";
+            case 4:
+                return "F ";
+            case 5:
+                return "S ";
+            case 6:
+                return "Su ";
+        }
+        return null;
+    }
 }
