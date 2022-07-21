932be4784bbc8cd38da0e1c1ec776b37fcf3a7fc
Cierra Ouellette <cierra3202@gmail.com>
2022-07-21 10:52:28 -0700

    Users can search in Feed View for farms/events.

--- app/src/main/java/com/example/locavore/Activities/MainActivity.java
+++ app/src/main/java/com/example/locavore/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.locavore.Activities;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -17,6 +18,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.example.locavore.Fragments.FarmProfileFragment;
@@ -74,31 +76,4 @@ public class MainActivity extends AppCompatActivity {
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.map_screen);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle presses on the action bar items
-        if (item.getItemId() == R.id.btnLogout) {
-            ParseUser.logOutInBackground(e -> {
-                if (e != null) {
-                    Log.e(TAG, "Issue with logout", e);
-                    Toast.makeText(MainActivity.this, "Issue with logout!", Toast.LENGTH_SHORT).show();
-                } else {
-                    //go to the login activity
-                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(i);
-                    Toast.makeText(MainActivity.this, "Success in logging out!", Toast.LENGTH_SHORT).show();
-                }
-            });
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
