f6651daa33751471ebaa40ce9a6568b38635c96b
Cierra Ouellette <cierra3202@gmail.com>
2022-07-22 14:07:50 -0700

    Bux fix / cleanup in signup/login.

--- app/src/main/java/com/example/locavore/AuthenticationManager.java
+++ app/src/main/java/com/example/locavore/AuthenticationManager.java
@@ -20,6 +20,7 @@ public class AuthenticationManager {
     public static final String TAG = "AuthenticationManager";
     public static final String STR_LOGIN = "login";
     public static final String STR_SIGNUP = "signup";
+    private static final double METERS_TO_MILE = 1609.34;
     public static final int USER_NONEXISTENT_ERROR = 101;
     public static final int USER_ALREADY_EXISTS_ERROR = 202;
     public static final int MIN_PASSWORD_LENGTH = 10;
@@ -29,8 +30,27 @@ public class AuthenticationManager {
         this.context = context;
     }
 
-    public boolean validateFields(ParseException e, String operation, String userType, String name, String emailAddress, String username, String password, String bio, String address, int radius) {
-        if(e != null ) { // a Parse error has occurred
+    public boolean validateFields(String operation, String userType, String name, String emailAddress, String username, String password, String bio, String address, int radius) {
+        if (Objects.equals(operation, STR_SIGNUP) && password.length() < MIN_PASSWORD_LENGTH) { // check password length (and complexity?)
+            Toast.makeText(context, context.getString(R.string.short_password), Toast.LENGTH_SHORT).show();
+            return false;
+        }  else if(name.isEmpty()) {
+            Toast.makeText(context, context.getString(R.string.empty_name), Toast.LENGTH_SHORT).show();
+        } else if(emailAddress.isEmpty()) { // check email is a valid email?
+            Toast.makeText(context, context.getString(R.string.empty_email), Toast.LENGTH_SHORT).show();
+        } else if(bio.isEmpty()) {
+            Toast.makeText(context, context.getString(R.string.empty_bio), Toast.LENGTH_SHORT).show();
+        } else if(address.isEmpty()) { // check if address is a valid address?
+            Toast.makeText(context, context.getString(R.string.empty_address), Toast.LENGTH_SHORT).show();
+        } else if(radius < 5 || radius > 25) {
+            Toast.makeText(context, context.getString(R.string.invalid_radius), Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+
+    public boolean checkParseErrors(ParseException e, String operation, String userType, String name, String emailAddress, String username, String password, String bio, String address, int radius) {
+        if (e != null) { // a Parse error has occurred
             if (username.isEmpty()) {
                 Toast.makeText(context, context.getString(R.string.username_field_empty), Toast.LENGTH_SHORT).show();
             } else if (password.isEmpty()) {
@@ -46,28 +66,14 @@ public class AuthenticationManager {
                 } else {
                     if (e.getCode() == USER_ALREADY_EXISTS_ERROR) {
                         Toast.makeText(context, context.getString(R.string.user_already_exists), Toast.LENGTH_SHORT).show();
-                    } /*else if(name.isEmpty()) {
-
-                } else if(emailAddress.isEmpty()) {
-
-                } else if(bio.isEmpty()) {
-
-                } else if(address.isEmpty()) {
-
-                } else if(radius == 0) {
-
-                }*/ else {
+                    } else {
                         Log.e(TAG, "Issue with signup " + e.getCode(), e);
                         Toast.makeText(context, context.getString(R.string.misc_signup_error), Toast.LENGTH_SHORT).show();
                     }
                 }
             }
             return false;
-        } else if (Objects.equals(operation, STR_SIGNUP) && password.length() < MIN_PASSWORD_LENGTH) { // check password complexity/length
-            Toast.makeText(context, context.getString(R.string.short_password), Toast.LENGTH_SHORT).show();
-            return false;
-        }  // check email is a valid email
-           // check if address is a valid address
+        }
         return true;
     }
 
@@ -80,20 +86,21 @@ public class AuthenticationManager {
         user.put(User.KEY_BIO, bio);
         user.put(User.KEY_ADDRESS, address);
         user.put(User.KEY_USER_TYPE, userType);
-        user.put(User.KEY_RADIUS, radius);
-        user.signUpInBackground(e -> {
-            if(validateFields(e, STR_SIGNUP, userType, name, emailAddress, username, password, bio, address, radius))
-            {
-                dataManager.mRadius = radius;
-                goToSplashScreenActivity((Activity) context);
-                Toast.makeText(context, context.getString(R.string.success), Toast.LENGTH_SHORT).show();
-            }
-        });
+        user.put(User.KEY_RADIUS, radius * METERS_TO_MILE);
+        if(validateFields(STR_SIGNUP, userType, name, emailAddress, username, password, bio, address, radius)) {
+            user.signUpInBackground(e -> {
+                if (checkParseErrors(e, STR_SIGNUP, userType, name, emailAddress, username, password, bio, address, radius)) {
+                    dataManager.mRadius = radius;
+                    goToSplashScreenActivity((Activity) context);
+                    Toast.makeText(context, context.getString(R.string.success), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 
     public void login(String username, String password) {
         ParseUser.logInInBackground(username, password, (user, e) -> {
-            if(validateFields(e, STR_LOGIN, "", "", "", username, password, "", "", 0))
+            if(checkParseErrors(e, STR_LOGIN, "", "", "", username, password, "", "", 0))
             {
                 dataManager.mRadius = ParseUser.getCurrentUser().getInt(User.KEY_RADIUS);
                 goToSplashScreenActivity((Activity) context);
